698c68092d195121ca3e2dcd5d316c9977fd735f
Sergey Malyshev <malyshevs_v@mail.rul>
2023-06-24 00:36:46 +0300

    initial commit

---
+++ src/main/java/com/example/jenkins_tamplate/JenkinsTamplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_tamplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTamplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTamplateApplication.class, args);
+    }
+
+}
